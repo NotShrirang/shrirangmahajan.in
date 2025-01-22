cb540814d6f8a8401e51bd0e45f42d6550352a83
NotShrirang <shrirangmahajan123@gmail.com>
2025-01-22 22:03:38 +0530

    feat: add menu bar

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from "react";
+import styles from "./MenuBar.module.css";
 
 const MenuBar = () => {
-  return <div></div>;
+  return (
+    <div className={styles.menuBarContainer}>
+      <div className={styles.menuBarContent}>
+        <div className={styles.menuComponent}>
+          <div className={styles.menuItem}>
+            <div>About</div>
+          </div>
+          <div className={styles.menuItem}>
+            <div>Projects</div>
+          </div>
+          <div className={styles.menuItem}>
+            <div>Experience</div>
+          </div>
+          <div className={styles.menuItem}>
+            <div>Education</div>
+          </div>
+          <div className={styles.menuItem}>
+            <div>Contact</div>
+          </div>
+        </div>
+        <div className={styles.trustedContainer}>
+          <div className={styles.trusted}>Trusted</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MenuBar;
