fd0779bd515ab5ca621209d515760760f1a6fabd
NotShrirang <shrirangmahajan123@gmail.com>
2025-01-23 23:59:12 +0530

    fix: themes

--- src/components/MarkdownCell/MarkdownCell.jsx
+++ src/components/MarkdownCell/MarkdownCell.jsx
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./MarkdownCell.module.css";
 
 const MarkdownCell = ({ cell, activeCell, setActiveCell }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+    setTheme(theme);
+  }, []);
   return (
     <div
       className={styles.cell}
+      style={{
+        border:
+          activeCell != cell.id
+            ? ""
+            : theme == "light"
+            ? "1px solid #ec7424"
+            : "1px solid #2196f3",
+      }}
       key={cell.id}
       onClick={(e) => {
         e.preventDefault();
--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -6,18 +6,10 @@ const MenuBar = () => {
     <div className={styles.menuBarContainer}>
       <div className={styles.menuBarContent}>
         <div className={styles.menuComponent}>
-          <a className={styles.menuItem} href="#projects">
-            Projects
-          </a>
-          <a className={styles.menuItem} href="#experience">
-            Experience
-          </a>
-          <a className={styles.menuItem} href="#education">
-            Education
-          </a>
-          <a className={styles.menuItem} href="#contact">
-            Contact
-          </a>
+          <div className={styles.menuItem}>Projects</div>
+          <div className={styles.menuItem}>Experience</div>
+          <div className={styles.menuItem}>Education</div>
+          <div className={styles.menuItem}>Contact</div>
         </div>
         <div className={styles.trustedContainer}>
           <div className={styles.trusted}>Trusted</div>
