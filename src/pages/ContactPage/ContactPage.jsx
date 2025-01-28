bfdd90a41986f80bd8a0dc83497a42cb4e536bba
NotShrirang <shrirangmahajan123@gmail.com>
2025-01-28 20:42:19 +0530

    feat: add contact me page

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -7,10 +7,38 @@ import getTheme from "../../utils/theme";
 const ContactPage = () => {
   const [activeCell, setActiveCell] = useState(1);
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     document.title = "Contact | Shrirang Mahajan";
   }, []);
+
+  const handleSubmit = () => {
+    const myForm = import.meta.env.VITE_FORMSPREE;
+    const url = `https://formspree.io/f/${myForm}`;
+    var formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("message", message);
+    fetch(url, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("Message sent successfully!");
+        } else {
+          alert("Message could not be sent. Please try again later.");
+        }
+      })
+      .catch((error) => {
+        alert("Message could not be sent. Please try again later.");
+      });
+  };
 
   return (
     <div className={styles.contactPage}>
@@ -44,6 +72,37 @@ const ContactPage = () => {
                       Or fill out the form below and I will get back to you as
                       soon as possible.
                     </div>
+                    <div className={styles.contactPageDescription}>
+                      <font className={styles.contactPageForm}>
+                        <div className={styles.contactPageFormFields}>
+                          Name:{" "}
+                          <input
+                            type="text"
+                            className={styles.contactPageInputField}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
+                        </div>
+                        <div className={styles.contactPageFormFields}>
+                          Email:{" "}
+                          <input
+                            type="email"
+                            className={styles.contactPageInputField}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </div>
+                        <div className={styles.contactPageFormFields}>
+                          Message:{" "}
+                          <textarea
+                            type="text"
+                            className={styles.contactPageInputField}
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                          />
+                        </div>
+                      </font>
+                    </div>
                   </div>
                 ),
               }}
@@ -57,14 +116,35 @@ const ContactPage = () => {
                   <div className={styles.markdownContainer}>
                     <div className={styles.contactPageDescription}>
                       <code className={styles.contactPageCode}>
-                        name = "
-                        <input
-                          type="text"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                          className={styles.contactPageInputField}
-                        />
-                        "
+                        <span>
+                          <span
+                            style={{
+                              color:
+                                getTheme() === "light" ? "#0f37bc" : "#64b5f6",
+                            }}
+                          >
+                            import
+                          </span>{" "}
+                          portfolio
+                          <br />
+                          <br />
+                          <span style={{ color: "#479AFF" }}>name</span> = "
+                          {name}"
+                          <br />
+                          <span style={{ color: "#479AFF" }}>email</span> = "
+                          {email}"
+                          <br />
+                          <span style={{ color: "#479AFF" }}>message</span> = "
+                          {message}"
+                          <br />
+                          <br />
+                          <span>portfolio</span>.
+                          <span style={{ color: "#479AFF" }}>send_message</span>
+                          (<span style={{ color: "#479AFF" }}>name</span>,{" "}
+                          <span style={{ color: "#479AFF" }}>email</span>,{" "}
+                          <span style={{ color: "#479AFF" }}>message</span>)
+                          <br />
+                        </span>
                       </code>
                     </div>
                   </div>
@@ -79,16 +159,19 @@ const ContactPage = () => {
                 output: (
                   <div className={styles.markdownContainer}>
                     <div className={styles.contactPageDescription}>
-                      <code className={styles.contactPageCode}>
-                        name = "
-                        <input
-                          type="text"
-                          value={name}
-                          className={styles.contactPageInputField}
-                          disabled
-                        />
-                        "
-                      </code>
+                      <button
+                        className={styles.contactPageButton}
+                        onClick={handleSubmit}
+                      >
+                        <img
+                          src={`https://img.icons8.com/?size=100&id=59862&format=png&color=${
+                            getTheme() == "dark" ? "BDBDBD" : "616161"
+                          }`}
+                          alt="Run this cell"
+                          className={styles.controlIcon}
+                        />{" "}
+                        Run Code
+                      </button>
                     </div>
                   </div>
                 ),
