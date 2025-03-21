28abf9f2b3046697266b906da91bddc829469b03
notshrirang <shrirangmahajan123@gmail.com>
2025-03-21 20:09:45 +0530

    fix: blog search bar behaviour

--- src/pages/Blogs/BlogsPage.jsx
+++ src/pages/Blogs/BlogsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import styles from "./BlogsPage.module.css";
 import BlogCard from "../../components/BlogCard/BlogCard";
 import blogs from "../../data/blogs.js";
@@ -7,24 +8,56 @@ import Footer from "../../components/Footer/Footer.jsx";
 const BlogsPage = () => {
   const [blogsList, setBlogsList] = useState([]);
   const [searchText, setSearchText] = useState("");
-  const [searchParams, setSearchParams] = useState(new URLSearchParams());
+  const [searchParams, setSearchParams] = useSearchParams();
   const searchQuery = searchParams.get("q") || "";
   const [tagsList, setTagsList] = useState([]);
 
   useEffect(() => {
-    setBlogsList(blogs);
-    const tags = [];
-    blogs.forEach((blog) => {
-      blog.tags.forEach((tag) => {
-        if (!tags.includes(tag)) {
-          tags.push(tag);
+    document.title = "Blogs | Shrirang Mahajan";
+    if (searchQuery !== "") {
+      const tempBlogs = blogs.filter((blog) => {
+        var found = blog.tags.some((tag) => {
+          return tag.toLowerCase().includes(searchQuery.toLowerCase());
+        });
+        if (found) {
+          return true;
         }
+        if (blog.title.toLowerCase().includes(searchQuery.toLowerCase())) {
+          return true;
+        }
+        if (
+          blog.content
+            .toString()
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        ) {
+          return true;
+        }
+        return false;
       });
-    });
-    setTagsList(tags);
-  }, []);
-
-  document.title = "Blogs | Shrirang Mahajan";
+      setBlogsList(tempBlogs);
+      const tags = [];
+      tempBlogs.forEach((blog) => {
+        blog.tags.forEach((tag) => {
+          if (!tags.includes(tag)) {
+            tags.push(tag);
+          }
+        });
+      });
+      setTagsList(tags);
+    } else {
+      setBlogsList(blogs);
+      const tags = [];
+      blogs.forEach((blog) => {
+        blog.tags.forEach((tag) => {
+          if (!tags.includes(tag)) {
+            tags.push(tag);
+          }
+        });
+      });
+      setTagsList(tags);
+    }
+  }, [searchQuery]);
 
   return (
     <div className={styles.BlogsPage}>
@@ -36,13 +69,12 @@ const BlogsPage = () => {
               <input
                 type="text"
                 className={styles.BlogsSearchBarInput}
-                placeholder="Search Projects (Keyword based)"
+                placeholder="Search Blogs (Keyword based)"
                 value={searchText}
                 onChange={(e) => {
                   setSearchText(e.target.value);
                   const newQuery = e.target.value;
                   const newParams = new URLSearchParams(searchParams);
-
                   if (newQuery) {
                     newParams.set("q", newQuery);
                   } else {
@@ -50,8 +82,8 @@ const BlogsPage = () => {
                   }
 
                   setSearchParams(newParams);
-                  const tempBlogs = blogs.filter((project) =>
-                    project.tags.some((tag) => {
+                  const tempBlogs = blogs.filter((blog) =>
+                    blog.tags.some((tag) => {
                       return tag.toLowerCase().includes(newQuery.toLowerCase());
                     })
                   );
@@ -67,10 +99,10 @@ const BlogsPage = () => {
                   if (filter === "All") {
                     setBlogsList(blogs);
                   } else {
-                    const tempProjects = blogs.filter(
-                      (blog) => blog.domain === filter
+                    const tempBlogs = blogs.filter((blog) =>
+                      blog.tags.includes(filter)
                     );
-                    setBlogsList(tempProjects);
+                    setBlogsList(tempBlogs);
                   }
                 }}
               >
